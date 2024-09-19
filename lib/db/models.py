21217f388e94bd10bb7cc44b4bb97cc4d0782303
Sara Achieng <achieng997@gmail.com>
2024-09-19 12:05:11 +0300

    Added a users table

--- lib/db/models.py
+++ lib/db/models.py
@@ -3,7 +3,17 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
 engine = create_engine('sqlite:///fitness_database.db')
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    workouts = relationship("Workout", back_populates="user")
