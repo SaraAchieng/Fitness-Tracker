2b270a2340d8f6fea0ce49536cd6546025c9bf04
Sara Achieng <achieng997@gmail.com>
2024-09-20 13:15:19 +0300

    Deleted unnecessary comments

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,44 +1,3 @@
-# # models.py
-
-
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime, ForeignKey
-# from sqlalchemy.orm import relationship
-# from datetime import datetime
-
-
-# engine = create_engine('sqlite:///fitness_database.db')
-
-# Base = declarative_base()
-
-# class User(Base):
-#     __tablename__ = 'users'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String, nullable=False)
-#     email = Column(String, nullable=False, unique=True)
-#     workouts = relationship("Workout", back_populates="user")
-    
-# class Workout(Base):
-#     __tablename__ = 'workouts'
-#     id = Column(Integer, primary_key=True)
-#     workout_type = Column(String, nullable=False)
-#     distance = Column(Float)  # Distance in kilometers
-#     steps = Column(Integer)  # Number of steps
-#     calories = Column(Float, nullable=False)  # Calories burned
-#     date = Column(DateTime, default=datetime.utcnow)
-#     user_id = Column(Integer, ForeignKey('users.id'))
-#     user = relationship("User", back_populates="workouts")   
-    
-# class Goal(Base):
-#     __tablename__ = 'goals'
-#     id = Column(Integer, primary_key=True)
-#     goal_type = Column(String, nullable=False)  # Type of goal (steps, calories, distance)
-#     target_value = Column(Float, nullable=False)  # Target value for the goal (e.g., 10000 steps)
-#     is_achieved = Column(Boolean, default=False)  # Whether the goal is achieved or not
-#     user_id = Column(Integer, ForeignKey('users.id'))  # Link to the user
-#     user = relationship("User", back_populates="goals")
-     
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String, Float, Boolean, Table, DateTime, ForeignKey
@@ -120,22 +79,6 @@ Base.metadata.create_all(engine)
 engine = create_engine('sqlite:///fitness_database.db')
 Session = sessionmaker(bind=engine)
 session = Session()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-# def create_user():
-#     name = input(Fore.GREEN + "Enter name: " + Style.RESET_ALL)
-#     email = input("Enter email: ")
-#     user = User(name=name, email=email)
-#     session.add(user)
-#     session.commit()
-#     print("User created.")
 
 
 def create_user():
@@ -167,21 +110,6 @@ def update_user():
         print("User updated.")
     else:
         print("User not found.")
-
-
-
-
-
-
-# def delete_user():
-#     user_id = int(input("Enter user ID to delete: "))
-#     user = session.query(User).filter_by(id=user_id).first()
-#     if user:
-#         session.delete(user)
-#         session.commit()
-#         print("User deleted.")
-#     else:
-#         print("User not found.")
 
 
 def delete_user():
@@ -276,13 +204,7 @@ def delete_workout():
     else:
         print("Workout not found.")
 
-# def display_all_workouts():
-#     workouts = session.query(Workout).all()
-#     if workouts:
-#         for workout in workouts:
-#             print(f"ID: {workout.id}, Type: {workout.workout_type}, Distance: {workout.distance} km, Steps: {workout.steps}, Calories: {workout.calories}")
-#     else:
-#         print("No workouts found.")
+
 
 def display_all_workouts():
     workouts = session.query(Workout).all()
